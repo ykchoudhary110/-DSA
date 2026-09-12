d28f3cd1fe41c9d5a04b07f4b61659ad84c8b5e2
ykchoudhary110 <145893475+ykchoudhary110@users.noreply.github.com>
2026-09-13 01:35:10 +0530

    Time: 539 ms (79.82%) | Memory: 22.3 MB (32.39%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,33 +2,38 @@ from typing import List
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        result = []
         n = len(nums)
+        res = []
 
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(n-2):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
 
-            left = i + 1
-            right = n - 1
-            target = -nums[i]
+            left = i+1
+            right = n-1
 
-            while left < right:
+            target = -1 * nums[i]
+
+            while left<right:
                 s = nums[left] + nums[right]
 
-                if s == target:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
+                if s==target:
+                    res.append([nums[i], nums[left],nums[right]])
 
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                    left+=1
+                    right-=1
 
-                elif s < target:
-                    left += 1
+                    while left<right and nums[left] == nums[left-1]:
+                        left+=1
+                    while left<right and nums[right] == nums[right+1]:
+                        right-=1
+
+                elif s<target:
+                    left+=1
+                    
                 else:
-                    right -= 1
+                    right-=1
+        return res
 
-        return result
+
+     
