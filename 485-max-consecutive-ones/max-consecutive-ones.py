14dd1d8aec3531d58eb560f5c6f7a4981fd30289
ykchoudhary110 <145893475+ykchoudhary110@users.noreply.github.com>
2026-09-19 10:53:09 +0530

    Time: 18 ms (46.25%) | Memory: 21.9 MB (44.06%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -5,7 +5,7 @@ class Solution:
         # left = 0
         # res = 0
 
-        # for right in range(len(nums)):
+        # for right in range(len(nums)):  two pointer sliding window solution 
         #     if nums[right]==0:
         #         left = right+1
 
@@ -16,7 +16,7 @@ class Solution:
         count = 0
         res = 0
         for num in nums:
-            if num == 1:
+            if num == 1:    #normal solution but littel better
                 count+=1
             else:
                 count =0
