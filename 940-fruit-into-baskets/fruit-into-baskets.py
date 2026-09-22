580bd545dff0d181d77003976f41d2590a11cf80
ykchoudhary110 <145893475+ykchoudhary110@users.noreply.github.com>
2026-09-23 01:16:33 +0530

    Time: 164 ms (84.98%) | Memory: 25.9 MB (35.24%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -11,7 +11,7 @@ class Solution:
                 freq[fruits[left]]-= 1
                 if freq[fruits[left]]==0:
                     del freq[fruits[left]]
-                left+=1
+                left+=1 #if not done then left wil be on zero always adn then right - left +1 will be wrong at any case
 
             res = max(res, right - left +1)
         return res
